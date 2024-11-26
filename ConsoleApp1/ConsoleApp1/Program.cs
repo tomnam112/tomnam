a748b7c0fa00b18108dcceaf32f28716b1f9553f
tomnam112 <worrapot.s@kkumail.com>
2024-11-26 11:14:37 +0700

    w

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Worrapot Saechua";
+string student_id = "673450040-3";
+//string is datatype 
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id: " + student_id);
